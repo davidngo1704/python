e0cc7e5b6f9a3adec2c2b25e9b61d04652ca39db
daint <davidngo1704@gmail.com>
2025-12-13 15:17:55 +0700

    d

--- app/main.py
+++ app/main.py
@@ -8,7 +8,6 @@ from app.routers import notification, common, chat, chromadb
 app = FastAPI(title="Thành Đại")
 
 Base.metadata.create_all(bind=engine)
-#app.include_router(common.router, prefix="/common", tags=["common"])
 
 app.include_router(notification.router, prefix="/notification", tags=["notification"])
 app.include_router(chat.router, prefix="/chat", tags=["chat"])
